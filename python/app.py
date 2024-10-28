71be3842b703a480481f7c8a16baedccebf951f9
Justin <rayjustin481@gmail.com>
2024-10-28 04:54:33 +0300

    i think i fixed the deployment error

--- python/app.py
+++ python/app.py
@@ -220,4 +220,4 @@ def delete_cart_item(product_id):
     return jsonify({'message': 'Cart item deleted'}), 200
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
